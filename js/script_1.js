a8eda9ab33183bfbbbfcb933c18dea66da1edc73
Laila Sumiya Khan <nilokhan207@gmail.com>
2022-01-02 15:36:12 +0600

    project push

---
+++ js/script_1.js
@@ -0,0 +1,9 @@
+var scroll = new SmoothScroll('.side-menu-text a', { speed: 700 });
+
+window.onscroll = function()
+{
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scroll = (winScroll / height) * 100;
+	document.getElementById("scrollBar").style.width = scroll + "%";
+};
